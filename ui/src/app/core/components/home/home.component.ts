bf13c47f9d4a77f62a9a503c827620d5bf15761a
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-11-02 19:50:18 -0300

    updates

--- ui/src/app/core/components/home/home.component.ts
+++ ui/src/app/core/components/home/home.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   moduleId: module.id.toString(),
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
+@HostListener('window:resize', ['$event'])
 export class HomeComponent implements OnInit {
+  public windowHeight: any = 0;
+
   constructor() { }
+  
 
   ngOnInit() {
-
+    this.windowHeight= window.innerHeight;
   }
+
+  onResize(event) {
+    this.windowHeight= event.target.innerHeight;
+  }
+
   public scrollToServices(): void {
       const element = document.querySelector("#services");
-      console.log(element)
       element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
