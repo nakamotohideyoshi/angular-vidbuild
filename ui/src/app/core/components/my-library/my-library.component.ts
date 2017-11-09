9e7b22e2f52438bb625c2d2186e3b8a3ade4b737
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-11-09 11:58:22 -0600

    implement multi-search feature on library page.

--- ui/src/app/core/components/my-library/my-library.component.ts
+++ ui/src/app/core/components/my-library/my-library.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Response} from '@angular/http';
 import {GettyService} from '../../../modules/shared/services/getty.service';
+import {MultiSearchSerivice} from '../../../modules/shared/services/multi-search.service';
+import {MultiSearchFilter} from '../../../modules/shared/pipes/multi-search-filter.pipe';
 import {EditorService} from '../../../modules/editor/services/editor.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -8,7 +10,11 @@ import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-my-library',
   templateUrl: './my-library.component.html',
-  styleUrls: ['./my-library.component.scss']
+  styleUrls: ['./my-library.component.scss'],
+  providers: [
+    MultiSearchSerivice,
+    MultiSearchFilter
+  ]
 })
 
 export class MyLibraryComponent implements OnInit, OnDestroy {
@@ -18,10 +24,14 @@ export class MyLibraryComponent implements OnInit, OnDestroy {
   sum = 5;
   total = 0;
   columns: String = 'container-img col-sm-6 col-md-3 col-lg-3';
+  searchItem: String = '';
+  itemList: any = [];
 
   constructor(private gettyService: GettyService,
               public editorService: EditorService,
-              private http: HttpClient ) {
+              private http: HttpClient,
+              private multiSearchService: MultiSearchSerivice,
+              private filterPipe: MultiSearchFilter ) {
   }
 
   ngOnInit() {
@@ -32,7 +42,8 @@ export class MyLibraryComponent implements OnInit, OnDestroy {
     this.gettyService.loadMovies(0, 10);
     this.post$ = this.gettyService.movies();
     this.gettyService.total().subscribe((total) => {
-      this.total = total;
+    this.total = total;
+    this.bindList();
     });
   }
 
@@ -58,6 +69,36 @@ export class MyLibraryComponent implements OnInit, OnDestroy {
 
   on4Columns() {
     this.columns = 'container-img col-sm-6 col-md-3 col-lg-3';
+  }
+
+
+  keyEvent(event) {
+    if (event.keyCode === 32) {
+      this.searchItem = this.searchItem.replace(' ', '');
+      if (this.searchItem.length > 0) {
+        this.onAdd();
+      }
+    }
+  }
+
+  onDelete(item) {
+    this.multiSearchService.deleteSearchItem(item);
+    this.bindList();
+  }
+  onAdd() {
+    this.multiSearchService.addSearchItem(this.searchItem);
+    this.bindList();
+    this.searchItem = '';
+  }
+
+  bindList() {
+    for (let index = 0 ; index < 5 ; index++ ) {
+      if (this.multiSearchService.searchItemList[index]) {
+        this.itemList[index] = this.multiSearchService.searchItemList[index];
+      } else {
+        this.itemList[index] = '';
+      }
+    }
   }
 
 }
