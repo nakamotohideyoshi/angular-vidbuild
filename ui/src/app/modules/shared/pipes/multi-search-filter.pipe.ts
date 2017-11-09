d8e20c4e58825d94060d9b48b7c1462460a7386a
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-11-09 13:35:10 -0600

    fix multi-search filter issues.

--- ui/src/app/modules/shared/pipes/multi-search-filter.pipe.ts
+++ ui/src/app/modules/shared/pipes/multi-search-filter.pipe.ts
@@ -31,13 +31,12 @@ export class MultiSearchFilter implements PipeTransform {
         });
       }
 
-        const filtered_items1 = parameter1 ? items.filter(item => item.title.indexOf(parameter1) !== -1 ) : [];
-        const filtered_items2 = parameter2 ? items.filter(item => item.title.indexOf(parameter2) !== -1 ) : [];
-        const filtered_items3 = parameter3 ? items.filter(item => item.title.indexOf(parameter3) !== -1 ) : [];
-        const filtered_items4 = parameter4 ? items.filter(item => item.title.indexOf(parameter4) !== -1 ) : [];
-        const filtered_items5 = parameter5 ? items.filter(item => item.title.indexOf(parameter5) !== -1 ) : [];
-        return parameter1 || parameter2 || parameter3 || parameter4 || parameter5 ?
-        filtered_items1.concat(filtered_items2, filtered_items3, filtered_items4, filtered_items5) : items;
+        const filtered_items1 = parameter1 ? items.filter(item => item.title.indexOf(parameter1) !== -1 ) : items;
+        const filtered_items2 = parameter2 ? filtered_items1.filter(item => item.title.indexOf(parameter2) !== -1 ) : filtered_items1;
+        const filtered_items3 = parameter3 ? filtered_items2.filter(item => item.title.indexOf(parameter3) !== -1 ) : filtered_items2;
+        const filtered_items4 = parameter4 ? filtered_items3.filter(item => item.title.indexOf(parameter4) !== -1 ) : filtered_items3;
+        const filtered_items5 = parameter5 ? filtered_items4.filter(item => item.title.indexOf(parameter5) !== -1 ) : filtered_items4;
+        return filtered_items5;
 
     }
 }
