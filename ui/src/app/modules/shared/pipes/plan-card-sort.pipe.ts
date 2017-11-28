a6821fe04e618b816e630eaea239215f02f6f264
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-11-28 13:24:51 -0600

    fix plan card ordering issue

--- ui/src/app/modules/shared/pipes/plan-card-sort.pipe.ts
+++ ui/src/app/modules/shared/pipes/plan-card-sort.pipe.ts
@@ -8,7 +8,7 @@ export class OrderBy implements PipeTransform {
  transform(items: any[]) {
      // ascending
   return Array.from(items).sort((item1: any, item2: any) => {
-    return this.orderByComparator(item1.price, item2.price);
+    return this.orderByComparator(item1.credits, item2.credits);
   });
  }
 
